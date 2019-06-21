8eb8db3575b9c0f68942a2b72401eb175be83367
Sistemas 2 <sistemas2@MacBook-Air-de-Sistemas.local>
2019-06-21 14:00:10 -0500

    changed path of templates to universal mode

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/sistemas2/projects/django/trydjango/src/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
