dcc41b3f93b82bdbd45d130b3299245741067637
jpazm <jaime@pazmental.mx>
2019-07-02 13:34:07 -0500

    played with form and post method

--- src/products/views.py
+++ src/products/views.py
@@ -3,14 +3,25 @@ from .models import Product
 from .forms import ProductForm
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm() # añadimos esto par poner la forma en blanco despues de enviar datos
-    context = {
-        'form': form
-    }
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == 'POST':
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)
+    context = {}
     return render(request, "products/product_create.html", context)
+
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm() # añadimos esto par poner la forma en blanco despues de enviar datos
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 
