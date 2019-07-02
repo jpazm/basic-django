183d3e9e7f745750f881ac67827df79e9619cb83
jpazm <jaime@pazmental.mx>
2019-07-02 12:01:31 -0500

    changed product templates to product app

--- src/products/views.py
+++ src/products/views.py
@@ -10,5 +10,5 @@ def product_detail_view(request):
     context = {
         'object': obj
     }
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
 
