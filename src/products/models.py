15447f113431616c112c0ad80fe9a7b7c2bd7031
jpazm <jaime@pazmental.mx>
2019-07-02 12:44:43 -0500

    created a form using ModelForm

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120) #max_length = required
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField(default=False)
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm() # añadimos esto par poner la forma en blanco despues de enviar datos
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
