cdac257e6e88a64bb3e871841316722ef1a82c71
jpazm <jaime@pazmental.mx>
2019-07-03 16:21:17 -0500

    another way to create a form

--- src/products/views.py
+++ src/products/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 def product_create_view(request):
-    # print(request.GET)
-    # print(request.POST)
-    if request.method == 'POST':
-        my_new_title = request.POST.get('title')
-        print(my_new_title)
-        # Product.objects.create(title=my_new_title)
-    context = {}
+    my_form = RawProductForm()
+    context = {
+        "form": my_form
+    }
     return render(request, "products/product_create.html", context)
+
+
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == 'POST':
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#         # Product.objects.create(title=my_new_title)
+#     context = {}
+#     return render(request, "products/product_create.html", context)
 
 
 # def product_create_view(request):
