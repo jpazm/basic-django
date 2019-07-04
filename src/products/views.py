ff61602c530cd4579766b4a9ce26937a9cf9ce2b
jpazm <jaime@pazmental.mx>
2019-07-04 17:26:45 -0500

    added a delete view

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm, RawProductForm
 from django.http import Http404
@@ -16,6 +16,16 @@ def dynamic_lookup_view(request, my_id):
     }
     return render(request, "products/product_detail.html", context)
 
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        # confirming delete
+        obj.delete()    
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_delete.html", context)
 
 
 def render_initial_data(request):
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view 
-from products.views import (product_detail_view, product_create_view, render_initial_data, dynamic_lookup_view)
+from products.views import (
+    product_detail_view, 
+    product_create_view, 
+    render_initial_data, 
+    dynamic_lookup_view,
+    product_delete_view
+    )
     
 
 
@@ -28,5 +34,6 @@ urlpatterns = [
     path('create/', render_initial_data),
     path('product/', product_detail_view),
     path('admin/', admin.site.urls),
-    path('products/<int:my_id>', dynamic_lookup_view, name='product') #int:my_id quiere decir integer, puede ser str(string) etc
+    path('products/<int:my_id>', dynamic_lookup_view, name='product'), #int:my_id quiere decir integer, puede ser str(string) etc
+    path('products/<int:id>/delete', product_delete_view, name='product-delete')
 ]
