27a717b6d5bbf117fe57702de481c7f76ac693cb
jpazm <jaime@pazmental.mx>
2019-07-05 17:57:58 -0500

    made a few adjunstments to make links work

--- src/products/models.py
+++ src/products/models.py
@@ -11,4 +11,4 @@ class Product(models.Model):
     featured = models.BooleanField(default=False)
 
     def get_absolute_url(self):
-        return "/products/{self.id}/"
+        return f"/products/{self.id}/"
--- src/products/views.py
+++ src/products/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm, RawProductForm
 from django.http import Http404
+from django.shortcuts import render, get_object_or_404, redirect
 
 def dynamic_lookup_view(request, id):
-    # obj = Product.objects.get(id=id)
     obj = get_object_or_404(Product, id=id)
     # la linea de arriba hace lo mismo que las siguientes comentadas
+    # obj = Product.objects.get(id=id)
     # try:
     #     obj = Product.objects.get(id=id)
     # except Product.DoesNotExist:
@@ -20,7 +21,7 @@ def product_delete_view(request, id):
     obj = get_object_or_404(Product, id=id)
     if request.method == "POST":
         # confirming delete
-        obj.delete()    
+        obj.delete()
         return redirect('../../')
     context = {
         "object": obj
